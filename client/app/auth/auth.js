78e5e8d2c18ed4770667884ed608b67d616ace1e
Michael Harris <mwharris333@gmail.com>
2015-05-04 00:57:07 -0700

    add auth controller, signup and signin view, button click from signup/signin views forwards user object to signin/signup methods of Auth Factory

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,4 +1,15 @@
 angular.module('courageousTrapeze.auth', [])
 
 .controller('AuthController', ['$scope', 'Auth', function($scope, Auth) {
+  $scope.user = {};
+  $scope.user.username = '';
+  $scope.user.password = '';
+
+  $scope.signin = function(){
+    Auth.signin($scope.user);
+  };
+
+  $scope.signup = function(){
+    Auth.signup($scope.user);
+  };
 }]);
