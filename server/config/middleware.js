680eb88a8f6a70bdb6d3a6ef4137480373c20ca4
Michael Harris <mwharris333@gmail.com>
2015-05-07 20:29:42 -0700

    append decoded token to req

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,11 +1,12 @@
 'use strict';
 var bodyParser = require('body-parser');
-
-var userRoutes = require('../users/userRoutes.js');
-var messageRoutes = require('../messages/messageRoutes.js');
-var contactRoutes = require('../contacts/contactRoutes.js');
+var authUtil = require('./authUtil.js');
 
 module.exports = function (app, express) {
+
+  var userRoutes = require('../users/userRoutes.js');
+  var messageRoutes = require('../messages/messageRoutes.js');
+  var contactRoutes = require('../contacts/contactRoutes.js');
 
   // instantiate express Routers for User, Message, and Contact 
   // see: http://expressjs.com/guide/routing.html
@@ -25,6 +26,16 @@ module.exports = function (app, express) {
   contactRoutes(contactRouter);
 
   // see: http://expressjs.com/api.html#app.use
+
+  app.use(express.static(__dirname + '/../../client'));
+  // see: http://expressjs.com/api.html re: app.use()
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+
+  // app.use('/api/users', authUtil.decodeTokenAttachToReq);
+  app.use('/api/messages', authUtil.decodeTokenAttachToReq);
+  app.use('/api/contacts', authUtil.decodeTokenAttachToReq);
+
   app.use('/api/users', userRouter);
   app.use('/api/messages', messageRouter);
   app.use('/api/contacts', contactRouter);
