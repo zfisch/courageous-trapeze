96be7bb7f7cea13eea6d5c0a016fe6ab16751716
Michael Harris <mwharris333@gmail.com>
2015-05-07 20:14:21 -0700

    store token to local storage

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -6,10 +6,31 @@ angular.module('courageousTrapeze.auth', [])
   $scope.user.password = '';
 
   $scope.signin = function(){
-    Auth.signin($scope.user);
+    Auth.signin($scope.user)
+    .then(function(token){
+      localStorage.setItem("courageousTrapeze", token);
+    })
+    .catch(function(err){
+      console.error(err);
+    });
   };
 
   $scope.signup = function(){
-    Auth.signup($scope.user);
+    Auth.signup($scope.user)
+    .then(function(token){
+      localStorage.setItem("courageousTrapeze", token);
+    })
+    .catch(function(err){
+      console.error(err);
+    });
   };
+
+  $scope.signout = function(){
+    Auth.signout($scope.user);
+  };
+
+  $scope.isAuth = function(){
+    return Auth.isAuth($scope.user);
+  };
+
 }]);
