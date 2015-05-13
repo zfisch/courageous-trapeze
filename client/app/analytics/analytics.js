e18d2504287d64cd94c1732f0ec2825a3b76dc02
brantchoate <brantchoate@gmail.com>
2015-05-12 19:06:38 -0700

    first go at d3 bars with dummy data

--- client/app/analytics/analytics.js
+++ client/app/analytics/analytics.js
@@ -1,7 +1,85 @@
 angular.module('courageousTrapeze.analytics', [])
 
-.controller('AnalyticsController', ['$scope', function($scope){
+.controller('AnalyticsController', ['$scope', function($scope) {
   $scope.clicks = [
-    { 'time': 1, 'value': 22 }
+    { 'time': 1, 'value': 22 },
+    { 'time': 2, 'value': 10 },
+    { 'time': 3, 'value': 1 },
+    { 'time': 4, 'value': 33 },
+    { 'time': 5, 'value': 21 }
+
   ];
-}]);
+}])
+
+.directive('d3Bar', [function() {
+  return {
+    restrict: 'E',
+    scope: {
+      data: '='
+    },
+    link: function(scope, el) {
+      var margin = {top: 50, right: 50, bottom: 50, left: 50};
+      var width = 1000 - margin.left - margin.right;
+      var height = 450 - margin.top - margin.bottom;
+
+      var svg = d3.select(el[0])
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+      var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+      var y = d3.scale.linear().range([height, 0]);
+
+      var xAxis = d3.svg.axis()
+          .scale(x)
+          .orient("bottom");
+      
+      var yAxis = d3.svg.axis()
+          .scale(y)
+          .orient("left")
+          .ticks(10);
+
+      scope.render = function(data) {
+        
+        x.domain(data.map(function(d) { return d.time; }));
+        y.domain([0, d3.max(data, function(d) { return d.value; })]);
+        
+        svg.selectAll('g.axis').remove();
+
+        svg.append('g')
+          .attr('class', 'x axis')
+          .attr('transform', 'translate(0,' + height + ')')
+          .call(xAxis);
+            
+        svg.append('g')
+          .attr('class', 'y axis')
+          .call(yAxis)
+          .append('text')
+            .attr('transform', 'rotate(-90)')
+            .attr('y', 6)
+            .attr('dy', '.71em')
+            .style('text-anchor', 'end');
+
+        var bars = svg.selectAll('.bar').data(data);
+        bars.enter()
+          .append('rect')
+          .attr('class', 'bar')
+          .attr('x', function(d) { return x(d.time); })
+          .attr('width', x.rangeBand());
+      
+        bars
+          .transition()
+          .duration(1000)
+          .attr('height', function(d) { return height - y(d.value); })
+          .attr('y', function(d) { return y(d.value); });
+      };
+
+      scope.$watch('data', function(){
+        scope.render(scope.data);
+      }, true);  
+    }
+  };
+}])
+;
