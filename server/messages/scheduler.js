b4dd120bd458b5076d8c06b6750c9d7ef86b2249
Zack Fischmann <zacharyrobert@gmail.com>
2015-05-14 14:20:30 -0700

    update scheduler and clean up unnecesary requires

--- server/messages/scheduler.js
+++ server/messages/scheduler.js
@@ -1,11 +1,6 @@
 var Agenda = require('agenda');
 var mongoose = require('mongoose');
-// Promise.promisifyAll(mongoose);
-// var Message = require('./messageModel.js');
-// var User = require('../users/userModel.js');
-// var Contact = require('../contacts/contactModel.js');
 // var twilio = require('../config/twilio.js');
-// 
 var User = mongoose.model('User');
 var Message = mongoose.model('Message');
 var Contact = mongoose.model('Contact');
@@ -17,29 +12,30 @@ agenda.define('send message', function(job, done) {
   console.log(data);
   var contactPhone = null;
   var userPhone = null;
-
+  
+  //get contact and user phone numbers from DB for twilio
   Contact.findById(data.contactId, function(err, data){
     contactPhone = data['phone'];
     User.findById(data.userId, function(err, data){
       userPhone = data['phone'];
       console.log(contactPhone, userPhone);
+      //TODO: send message through twilio
+      // twilio.sendMessage({
+      //   to: contactPhone,
+      //   from: '+14153196800',
+      //   body: data.text
+      // }, function(err, responseData){
+      //   if(err){
+      //     twilio.sendMessage({
+      //       to: userPhone,
+      //       from: '+14153196800',
+      //       body: 'Your message failed to send :('
+      //     });
+      //   }
+      // });
     });
   });
 
-  //TODO: send message through twilio
-  // twilio.sendMessage({
-  //   to: Contact.get(phone),
-  //   from: '+14153196800',
-  //   body: data.text
-  // }, function(err, responseData){
-  //   if(err){
-  //     twilio.sendMessage({
-  //       to: User.get(phone),
-  //       from: '+14153196800',
-  //       body: 'Your message failed to send :('
-  //     });
-  //   }
-  // });
 
 
   done();
