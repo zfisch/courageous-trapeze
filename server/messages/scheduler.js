0ff2352b87bf66dfdb455c13e73573b71406f23b
Zack Fischmann <zacharyrobert@gmail.com>
2015-05-13 19:50:57 -0700

    implement twilio sendmessage, needs review

--- server/messages/scheduler.js
+++ server/messages/scheduler.js
@@ -1,12 +1,29 @@
 var Agenda = require('agenda');
-var Message = require('./messageModel.js')
+var Message = require('./messageModel.js');
+var User = require('./userModel.js');
+var Contact = require('./contactModel.js');
+var twilio = require('./config/twilio.js');
 
 var agenda = new Agenda({db: { address: 'localhost:27017/courageoustrapezedb'}});
 
 agenda.define('send message', function(job, done) {
-  //TODO: send post to Twillio
   var data = job.attrs.data;
   console.log(data);
+
+  //send message through twilio
+  twilio.sendMessage({
+    to: Contact.get(phone),
+    from: '+14153196800',
+    body: data.text
+  }, function(err, responseData){
+    if(err){
+      twilio.sendMessage({
+        to: User.get(phone),
+        from: '+14153196800',
+        body: 'Your message failed to send :('
+      });
+    }
+  });
   done();
 });
 
