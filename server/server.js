465915de51286b57a1223c585a1d61b247eacdb3
cwhwang1986 <cwhwang1986@gmail.com>
2015-05-14 13:44:51 -0700

    Include twilio module in package.json, modify the link in index html

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var middleware = require('./config/middleware.js');
+var client = require('../twilio.js');
 
 var app = express();
 
