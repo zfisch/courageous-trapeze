7e7c37f7a35730353b3b4a5413a36245bb372c1c
Zack Fischmann <zacharyrobert@gmail.com>
2015-05-14 12:05:24 -0700

    grab user and contact phone numbers for twilio send message functions

--- server/messages/scheduler.js
+++ server/messages/scheduler.js
@@ -1,29 +1,47 @@
 var Agenda = require('agenda');
-var Message = require('./messageModel.js');
-var User = require('./userModel.js');
-var Contact = require('./contactModel.js');
-var twilio = require('./config/twilio.js');
+var mongoose = require('mongoose');
+// Promise.promisifyAll(mongoose);
+// var Message = require('./messageModel.js');
+// var User = require('../users/userModel.js');
+// var Contact = require('../contacts/contactModel.js');
+// var twilio = require('../config/twilio.js');
+// 
+var User = mongoose.model('User');
+var Message = mongoose.model('Message');
+var Contact = mongoose.model('Contact');
 
 var agenda = new Agenda({db: { address: 'localhost:27017/courageoustrapezedb'}});
 
 agenda.define('send message', function(job, done) {
   var data = job.attrs.data;
   console.log(data);
+  var contactPhone = null;
+  var userPhone = null;
 
-  //send message through twilio
-  twilio.sendMessage({
-    to: Contact.get(phone),
-    from: '+14153196800',
-    body: data.text
-  }, function(err, responseData){
-    if(err){
-      twilio.sendMessage({
-        to: User.get(phone),
-        from: '+14153196800',
-        body: 'Your message failed to send :('
-      });
-    }
+  Contact.findById(data.contactId, function(err, data){
+    contactPhone = data['phone'];
+    User.findById(data.userId, function(err, data){
+      userPhone = data['phone'];
+      console.log(contactPhone, userPhone);
+    });
   });
+
+  //TODO: send message through twilio
+  // twilio.sendMessage({
+  //   to: Contact.get(phone),
+  //   from: '+14153196800',
+  //   body: data.text
+  // }, function(err, responseData){
+  //   if(err){
+  //     twilio.sendMessage({
+  //       to: User.get(phone),
+  //       from: '+14153196800',
+  //       body: 'Your message failed to send :('
+  //     });
+  //   }
+  // });
+
+
   done();
 });
 
